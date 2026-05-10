04e87b463e660cce0a07b3dcb7ac02525c35d86b
Paulo <Pv75440@gmail.com>
2026-05-10 11:53:30 -0300

    Correção de botão de perfil na index

--- public/js/login.js
+++ public/js/login.js
@@ -35,7 +35,7 @@ function logar() {
     localStorage.setItem("usuarioLogin", encontrado.usuario);
     localStorage.setItem("usuarioPerfil", encontrado.perfil);
 
-    window.location.href = "home.html";
+    window.location.href = "../index.html";
   } else {
     erroEl.textContent = "Usuário ou senha inválidos.";
     document.getElementById("senha").value = "";
@@ -50,6 +50,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   if (localStorage.getItem("logado") === "true") {
-    window.location.href = "public/index.html";
+    window.location.href = "../index.html";
   }
 });
